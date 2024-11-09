2a1343f6fa8975367fbafdde8651364fe0a6799c
Adrianawang <carolinaadriana77@outlook.com>
2024-11-09 13:45:07 -0300

    1 atualização do app hotel
    1 atualização do app hotel

---
+++ HospedagemContratada.xaml.cs
@@ -0,0 +1,13 @@
+namespace MauiAppHotel.Views;
+
+public partial class HospedagemContratada : ContentPage
+{
+	public HospedagemContratada()
+	{
+		InitializeComponent();
+	}
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+}
